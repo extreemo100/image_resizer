769f24b01b20484d55baf68935a83475d28ad647
extreemo100 <58828956+extreemo100@users.noreply.github.com>
2025-08-22 02:56:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -141,4 +141,5 @@ def resize_images():
         )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
